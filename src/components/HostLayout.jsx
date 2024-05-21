4780890ce4ab0bd61dd1f60ab096b4400b8f9137
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-05-21 11:12:23 -0700

    sub nav active styles

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export const HostLayout = () => {
+
+    const hostStlyes = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
     return(
         <>
             <nav className="host-nav">
-                <Link to="/host">Dashboard</Link>
-                <Link to="/host/income">Income</Link>
-                <Link to="/host/reviews">Reviews</Link>
+                <NavLink end style={({ isActive }) => isActive ? hostStlyes : null} to="/host">Dashboard</NavLink>
+                <NavLink style={({ isActive }) => isActive ? hostStlyes : null} to="/host/income">Income</NavLink>
+                <NavLink style={({ isActive }) => isActive ? hostStlyes : null} to="/host/reviews">Reviews</NavLink>
             </nav>
             <Outlet />
         </>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 export const Nav = () => {
     return(
         <header>
             <Link className="site-logo" to="/">#VANLIFE</Link>
             <nav>
-                <Link to="/host">Host</Link>
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
+                <NavLink className={({isActive}) => isActive ? "active-link" : null} to="/host">Host</NavLink>
+                <NavLink className={({isActive}) => isActive ? "active-link" : null} to="/about">About</NavLink>
+                <NavLink className={({isActive}) => isActive ? "active-link" : null} to="/vans">Vans</NavLink>
             </nav>
         </header>
     );
